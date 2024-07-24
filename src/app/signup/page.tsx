def0eed1f93e9132988911b25b4690cb63447e65
Ronan O'Leary <roleary81@gmail.com>
2024-07-24 17:04:57 +0200

    sweet jesus

--- src/app/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignupForm } from "@/app/signup/_components/signup";
-
-export default async function SignupPage() {
-  return (
-    <div className="flex items-center justify-center p-10">
-      <SignupForm  />
-    </div>
-  );
-}
