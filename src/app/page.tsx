080663b69992c942d39880c09f08e7d3b24eef23
Ronan O'Leary <roleary81@gmail.com>
2024-07-24 16:33:28 +0200

    that was an ordeal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
-import DeployButton from "../components/DeployButton";
-import AuthButton from "../components/AuthButton";
+import AuthButton from "./../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
 import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
 import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
@@ -23,7 +22,7 @@ export default async function Index() {
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <DeployButton />
+          
           {isSupabaseConnected && <AuthButton />}
         </div>
       </nav>
@@ -45,8 +44,9 @@ export default async function Index() {
             className="font-bold hover:underline"
             rel="noreferrer"
           >
-            Ronanio
+            Ronan.
           </a>
+          Testing the pipeline.
         </p>
       </footer>
     </div>
